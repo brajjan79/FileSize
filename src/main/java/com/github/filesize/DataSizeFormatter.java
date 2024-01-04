2b5c003aca5ba9f2e98abdc2316671f3efab7b6a
brajjan79 <brajjan79@gmail.com>
2024-01-04 16:08:34 +0100

    Better implementation

--- src/main/java/com/github/filesize/DataSizeFormatter.java
+++ src/main/java/com/github/filesize/DataSizeFormatter.java
@@ -4,40 +4,51 @@ import java.util.Locale;
 
 public class DataSizeFormatter {
 
-    private static int defaultDecimalPlaces = 2;
+    private static int defaultMaxWidth = 4;
+    private static int defaultMaxDecimals = 2;
 
     /**
-     * Format bytes to a human readable String converted to most fit data size format. Default: 2 decimals
+     * Format bytes to a human readable String converted to most fit data size format.
      *
-     * Example: 1024 bytes will be "1.00KB" 1048576 bytes will be "1.00MB"
+     * Default: 4 max width and 2 decimals
+     *
+     * Example: 1024 bytes will be "    1 KB" 1048576 bytes will be "  1.0 MB"
      *
      * @param bytes :: size in bytes
      * @return String
      */
     public static String formatBytes(double bytes) {
-        return formatBytes(bytes, defaultDecimalPlaces);
+        return formatBytes(bytes, defaultMaxWidth, defaultMaxDecimals);
     }
 
     /**
-     * Format bytes to a human readable String converted to most fit data size format.
+     * Format bytes to a human-readable String converted to the most fit data size format.
      *
-     * Example: 1024 bytes will be "1.00KB" 1048576 bytes will be "1.00MB"
-     *
-     * @param bytes         :: size in bytes
-     * @param decimalPlaces :: Number of decimals to use
+     * @param bytes       :: size in bytes
+     * @param maxWidth    :: Max string with (excluding space + KB, MB ...)
+     * @param maxDecimals :: Maximum number of decimals to use
      * @return String
      */
-    public static String formatBytes(double bytes, int decimalPlaces) {
+    public static String formatBytes(double bytes, int maxWidth, int maxDecimals) {
         final String[] units = { "b", "KB", "MB", "GB", "TB", "PB" };
         int unitIndex = 0;
-
         double transformedBytes = bytes;
+
         while (transformedBytes >= 1024 && unitIndex < units.length - 1) {
             transformedBytes /= 1024;
             unitIndex++;
         }
 
-        final String formatSpecifier = "%." + decimalPlaces + "f %s";
-        return String.format(Locale.US, formatSpecifier, transformedBytes, units[unitIndex]);
+        final String numberFormat = unitIndex >= 2 ? "%." + getMaxDecimals(maxWidth, transformedBytes, maxDecimals) + "f" : "%.0f";
+        final String formatSpecifier = "%" + (maxWidth + 1) + "s %s";
+        final String formattedNumber = String.format(Locale.US, numberFormat, transformedBytes);
+
+        return String.format(Locale.US, formatSpecifier, formattedNumber, units[unitIndex]);
     }
+
+    private static int getMaxDecimals(int maxWidth, double number, int maxDecimals) {
+        final int length = String.valueOf((int) Math.abs(number)).length();
+        return maxWidth - length > maxDecimals ? maxDecimals : maxWidth - length;
+    }
+
 }
